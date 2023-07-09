53261326cb597fe8f63fd2db1b5feda3ee8b9ebc
Jared Becker <jaredbecker46@gmail.com>
2023-07-09 13:38:13 +0200

    - Put loader into separate component Also setup a new route and added the component for it

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,23 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './pages/home/home.component';
+import { AnimeDetailsComponent } from './pages/anime-details/anime-details.component';
 
-const routes: Routes = [{
-    path: '',
-    component: HomeComponent
-}];
+const routes: Routes = [
+    { path: '', component: HomeComponent },
+    {
+        path: 'anime',
+        children: [
+            {
+                path: ':anime-name',
+                component: AnimeDetailsComponent
+            }
+        ]
+    }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
