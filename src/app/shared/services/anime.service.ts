2e44b26618fee090486fbb1cfde236c8d15e1f5d
Jared Becker <jaredbecker46@gmail.com>
2023-07-09 13:37:24 +0200

    - Clean up anime service to be more DRY

--- src/app/shared/services/anime.service.ts
+++ src/app/shared/services/anime.service.ts
@@ -10,14 +10,11 @@ import { CategoryResponse } from '../models/category-response.interface';
     providedIn: 'root'
 })
 export class AnimeService {
+    private api: string = 'https://kitsu.io/api/edge';
     private anime_stream_map = new Map<string, Observable<AnimeResponse>>();
     private category_stream_map = new Map<string, Observable<CategoryResponse>>();
 
-    private api: string = 'https://kitsu.io/api/edge';
-
-    constructor(
-        private http: HttpClient,
-    ) { }
+    constructor(private http: HttpClient) { }
 
     /**
      * Gets the top 10 trending anime
@@ -25,15 +22,9 @@ export class AnimeService {
      * @returns Trending anime stream
      */
     public getTrendingAnime() {
-        if (!this.anime_stream_map.has('trending')) {
-            this.storeAnimeStream('trending', `${this.api}/trending/anime?limit=10`)
-        }
-
-        const $stream = this.anime_stream_map.get('trending');
-
-        if (!$stream) {
-            return throwError(() => new Error('No trending anime stream found'));
-        }
+        const url = `${this.api}/trending/anime?limit=10`;
+        const error = 'No trending anime stream found';
+        const $stream = this.checkAnimeStream('trending', url, error);
 
         return $stream;
     }
@@ -44,15 +35,9 @@ export class AnimeService {
      * @returns Highest rated anime stream
      */
     public getHighestRatedAnime() {
-        if (!this.anime_stream_map.has('highest_rated')) {
-            this.storeAnimeStream('highest_rated', `${this.api}/anime?page[limit]=10&sort=-average_rating`)
-        }
-
-        const $stream = this.anime_stream_map.get('highest_rated');
-
-        if (!$stream) {
-            return throwError(() => new Error('No highest rated anime stream found'));
-        }
+        const url = `${this.api}/anime?page[limit]=10&sort=-average_rating`;
+        const error = 'No highest rated anime stream found';
+        const $stream = this.checkAnimeStream('highest_rated', url, error);
 
         return $stream;
     }
@@ -63,15 +48,9 @@ export class AnimeService {
      * @returns Top Airing anime stream
      */
     public getTopAiringAnime() {
-        if (!this.anime_stream_map.has('top_airing')) {
-            this.storeAnimeStream('top_airing', `${this.api}/anime?filter[status]=current&page[limit]=10&sort=-user_count`)
-        }
-
-        const $stream = this.anime_stream_map.get('top_airing');
-
-        if (!$stream) {
-            return throwError(() => new Error('No top airing anime stream found'));
-        }
+        const url = `${this.api}/anime?filter[status]=current&page[limit]=10&sort=-user_count`;
+        const error = 'No top airing anime stream found';
+        const $stream = this.checkAnimeStream('top_airing', url, error);
 
         return $stream;
     }
@@ -81,16 +60,10 @@ export class AnimeService {
      *
      * @returns Top Airing anime stream
      */
-    public getTopUpcomingAnime() {
-        if (!this.anime_stream_map.has('top_upcoming')) {
-            this.storeAnimeStream('top_upcoming', `${this.api}/anime?filter[status]=upcoming&page[limit]=10&sort=-user_count`)
-        }
-
-        const $stream = this.anime_stream_map.get('top_upcoming');
-
-        if (!$stream) {
-            return throwError(() => new Error('No top upcoming anime stream found'));
-        }
+    public getTopUpcomingAnime(): Observable<AnimeResponse> {
+        const url = `${this.api}/anime?filter[status]=upcoming&page[limit]=10&sort=-user_count`;
+        const error = 'No top upcoming anime stream found';
+        const $stream = this.checkAnimeStream('top_upcoming', url, error);
 
         return $stream;
     }
@@ -100,16 +73,10 @@ export class AnimeService {
      *
      * @returns Top Airing anime stream
      */
-    public getMostPopularAnime() {
-        if (!this.anime_stream_map.has('most_popular')) {
-            this.storeAnimeStream('top_upcoming', `${this.api}/anime?page[limit]=10&sort=-user_count`)
-        }
-
-        const $stream = this.anime_stream_map.get('top_upcoming');
-
-        if (!$stream) {
-            return throwError(() => new Error('No top upcoming anime stream found'));
-        }
+    public getMostPopularAnime(): Observable<AnimeResponse> {
+        const url = `${this.api}/anime?page[limit]=10&sort=-user_count`;
+        const error = 'No most popular anime stream found';
+        const $stream = this.checkAnimeStream('most_popular', url, error);
 
         return $stream;
     }
@@ -120,20 +87,69 @@ export class AnimeService {
      * @returns Anime categories stream
      */
     public getAnimeCategories(): Observable<CategoryResponse> {
-        if (!this.category_stream_map.has('anime_categories')) {
-            const $stream = this.http
-                .get<AnimeResponse>(`${this.api}/categories?page[limit]=40&sort=-total_media_count`)
-                .pipe(
-                    shareReplay(1)
-                );
+        const url = `${this.api}/categories?page[limit]=40&sort=-total_media_count`;
+        const error = 'No anime with the provided name could be found.';
+        const $stream = this.checkCategoryStream('anime_categories', url, error);
 
-            this.category_stream_map.set('anime_categories', $stream);
+        return $stream;
+    }
+
+    /**
+     * Gets anime info for the requested name
+     *
+     * @param anime_name The slug prop of an anime response. IE: one pice -> one-piece
+     *
+     * @returns Anime stream
+     */
+    public getAnime(anime_name: string) {
+        const url = `${this.api}/anime/${anime_name}`;
+        const error = 'No anime with the provided name could be found.';
+        const $stream = this.checkAnimeStream(anime_name, url, error);
+
+        return $stream;
+    }
+
+    /**
+     * Checks the anime stream for an existing request and creates one if nothing is found
+     *
+     * @param key They key to lookup in the map
+     * @param url The URL of the request that needs to be made
+     * @param error_msg An error message to display if getting the stream fails
+     *
+     * @returns Stream of the requested API call
+     */
+    private checkAnimeStream(key: string, url: string, error_msg: string): Observable<AnimeResponse> {
+        if (!this.anime_stream_map.has(key)) {
+            this.storeAnimeStream(key, url);
         }
 
-        const $stream = this.category_stream_map.get('anime_categories');
+        const $stream = this.anime_stream_map.get(key);
 
         if (!$stream) {
-            return throwError(() => new Error('No anime categories stream found'));
+            return throwError(() => new Error(error_msg));
+        }
+
+        return $stream;
+    }
+
+    /**
+     * Checks the category stream for an existing request and creates one if nothing is found
+     *
+     * @param key They key to lookup in the map
+     * @param url The URL of the request that needs to be made
+     * @param error_msg An error message to display if getting the stream fails
+     *
+     * @returns Stream of the requested API call
+     */
+    private checkCategoryStream(key: string, url: string, error_msg: string): Observable<CategoryResponse> {
+        if (!this.category_stream_map.has(key)) {
+            this.storeCategoryStream(key, url);
+        }
+
+        const $stream = this.category_stream_map.get(key);
+
+        if (!$stream) {
+            return throwError(() => new Error(error_msg));
         }
 
         return $stream;
@@ -154,14 +170,33 @@ export class AnimeService {
             );
     }
 
+    private getCategoryStream(url: string) {
+        return this.http
+            .get<CategoryResponse>(url)
+            .pipe(
+                shareReplay(1)
+            );
+    }
+
     /**
      * Stores an anime stream
      *
      * @param key The key to store the stream under in the hash map
      * @param url The URL of the endpoint to query
      */
-    private storeAnimeStream(key: string, url: string) {
+    private storeAnimeStream(key: string, url: string): void {
         const $anime_stream = this.getAnimeStream(url);
         this.anime_stream_map.set(key, $anime_stream);
+    }
+
+    /**
+     * Stores a category stream
+     *
+     * @param key The key to store the stream under in the hash map
+     * @param url The URL of the endpoint to query
+     */
+    private storeCategoryStream(key: string, url: string): void {
+        const $category_stream = this.getCategoryStream(url);
+        this.category_stream_map.set(key, $category_stream);
     }
 }
