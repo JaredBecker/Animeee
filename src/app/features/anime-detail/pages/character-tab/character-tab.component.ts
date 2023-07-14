add5450a4cffe7f9f8aa74ab39e78d2fa060c5d2
Jared Becker <jaredbecker46@gmail.com>
2023-07-15 01:44:39 +0200

    - Add character page

--- src/app/features/anime-detail/pages/character-tab/character-tab.component.ts
+++ src/app/features/anime-detail/pages/character-tab/character-tab.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+import { Subscription } from 'rxjs';
+
+import { AnimeService } from '@shared/services/anime.service';
+import { AnimeDetailService } from '@shared/services/anime-detail.service';
 
 @Component({
   selector: 'app-character-tab',
   templateUrl: './character-tab.component.html',
   styleUrls: ['./character-tab.component.scss']
 })
-export class CharacterTabComponent {
+export class CharacterTabComponent implements OnInit, OnDestroy {
+    public is_loading: boolean = true;
+    public characters: any[] = [];
 
+    private character_subscription?: Subscription;
+
+    constructor(
+        private animeService: AnimeService,
+        private animeDetailService: AnimeDetailService,
+    ) { }
+
+    public ngOnInit(): void {
+        const anime = this.animeDetailService.getCurrentAnime();
+
+        if (anime) {
+            this.character_subscription = this.animeService.getCharacterInfo(anime.id, -1)
+                .subscribe({
+                    next: (characters_res) => {
+                        console.log(characters_res);
+
+                        if (characters_res.included) {
+                           this.characters = characters_res.included;
+                        }
+
+                        this.is_loading = false;
+                    },
+                    error: (err) => {
+                        console.error('Error fetching characters', err);
+                        this.is_loading = false;
+                    }
+                })
+        }
+    }
+
+    public ngOnDestroy(): void {
+        this.character_subscription?.unsubscribe();
+    }
 }
