08e61da17baaeffe0ddcc576d5cee072581fa6b2
Jared Becker <jaredbecker46@gmail.com>
2023-07-15 03:30:59 +0200

    - Add episode view

--- src/app/features/anime-detail/pages/episode-tab/episode-tab.component.ts
+++ src/app/features/anime-detail/pages/episode-tab/episode-tab.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+import { Subscription } from 'rxjs';
+
+import { AnimeService } from '@shared/services/anime.service';
+import { AnimeDetailService } from '@shared/services/anime-detail.service';
 
 @Component({
-  selector: 'app-episode-tab',
-  templateUrl: './episode-tab.component.html',
-  styleUrls: ['./episode-tab.component.scss']
+    selector: 'app-episode-tab',
+    templateUrl: './episode-tab.component.html',
+    styleUrls: ['./episode-tab.component.scss']
 })
-export class EpisodeTabComponent {
+export class EpisodeTabComponent implements OnInit, OnDestroy {
+    public episodes: any[] = [];
+    public is_loading: boolean = true;
 
+    private episode_subscription?: Subscription;
+
+    constructor(
+        private animeService: AnimeService,
+        private animeDetailService: AnimeDetailService,
+    ) { }
+
+    public ngOnInit(): void {
+        const anime = this.animeDetailService.getCurrentAnime();
+
+        if (anime) {
+            this.episode_subscription = this.animeService.getEpisodes(anime.id)
+            .subscribe({
+                next: (episode_res) => {
+                    console.log(episode_res.data);
+                    if (episode_res.data.length > 0) {
+                        this.episodes = episode_res.data;
+                    }
+
+                    this.is_loading = false;
+                },
+                error: (err) => {
+                    console.error('Error fetching episode stream', err);
+                    this.is_loading = false;
+                }
+            })
+        }
+    }
+
+    public ngOnDestroy(): void {
+        this.episode_subscription?.unsubscribe();
+    }
 }
